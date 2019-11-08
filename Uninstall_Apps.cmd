74ab379f5d369035de1670b2a6bf9c5a6d31e9d2
dextruder <51915070+dextruder@users.noreply.github.com>
2019-11-08 11:06:01 +0300

    rem

--- Uninstall_Apps.cmd
+++ Uninstall_Apps.cmd
@@ -3,7 +3,7 @@ adb.exe shell pm clear  --user 0 com.facebook.services
 adb.exe shell pm clear  --user 0 com.facebook.katana
 adb.exe shell pm clear  --user 0 com.facebook.system
 adb.exe shell pm clear  --user 0 com.facebook.appmanager
-:: прочий хлам ::
+:: РїСЂРѕС‡РёР№ С…Р»Р°Рј ::
 adb.exe shell pm clear  --user 0 com.samsung.android.app.sbrowseredge
 adb.exe shell pm clear  --user 0 com.android.wallpaper.livepicker
 adb.exe shell pm clear  --user 0 com.google.android.apps.docs
@@ -14,4 +14,3 @@ adb.exe shell pm clear  --user 0 com.sec.android.app.sbrowser
 adb.exe shell pm clear  --user 0 com.sec.android.app.shealth
 adb.exe shell pm clear  --user 0 com.linkedin.android
 adb.exe shell pm clear  --user 0 com.microsoft.appmanager
-
