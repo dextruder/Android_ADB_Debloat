8123c8bd27c50cd2bb21fc2f6114ed1101aab55b
dextruder <51915070+dextruder@users.noreply.github.com>
2019-11-08 11:04:58 +0300

    -facebook & other junk

---
+++ Uninstall_Apps.cmd
@@ -0,0 +1,17 @@
+:: Facebook ::
+adb.exe shell pm clear  --user 0 com.facebook.services
+adb.exe shell pm clear  --user 0 com.facebook.katana
+adb.exe shell pm clear  --user 0 com.facebook.system
+adb.exe shell pm clear  --user 0 com.facebook.appmanager
+:: прочий хлам ::
+adb.exe shell pm clear  --user 0 com.samsung.android.app.sbrowseredge
+adb.exe shell pm clear  --user 0 com.android.wallpaper.livepicker
+adb.exe shell pm clear  --user 0 com.google.android.apps.docs
+adb.exe shell pm clear  --user 0 com.sec.spp.push
+adb.exe shell pm clear  --user 0 com.samsung.android.app.spage
+adb.exe shell pm clear  --user 0 com.samsung.android.voc
+adb.exe shell pm clear  --user 0 com.sec.android.app.sbrowser
+adb.exe shell pm clear  --user 0 com.sec.android.app.shealth
+adb.exe shell pm clear  --user 0 com.linkedin.android
+adb.exe shell pm clear  --user 0 com.microsoft.appmanager
+
